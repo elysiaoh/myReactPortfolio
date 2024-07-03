3948a30d30f3acaae56cb3c51d66e80f15e8bc2f
Elysia Oh <102720577+elysiaoh@users.noreply.github.com>
2024-07-03 16:13:22 -0700

    Created Hero Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
-      <div className="">
-        <h1>Hello, Portfolio!</h1>
+      <div className="max-w-7xl w-full">
+        <Hero />
       </div>
     </main>
   );
